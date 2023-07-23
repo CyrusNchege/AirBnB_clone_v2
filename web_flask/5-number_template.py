5659059582f2549ae7142ce71cb40ec26d1ea814
CyrusNchege <cyruschegecc@gmail.com>
2023-07-23 12:43:40 +0300

    Add a route that takes an int and mapping it to a template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Starts a flask web app"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return "C {}".format(text.replace("_", " "))
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """prints Python is cool"""
+    text = text.replace("_", " ")
+    return "Python %s" % text
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """prints n is a number if n is an integer"""
+    return "%d is a number" % n
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """displays an HTML page only if n is an integer"""
+    return render_template("5-number.html", number=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
