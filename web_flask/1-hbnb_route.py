747bec9dc4f38d867fdccc200f60a89063b5d2b6
CyrusNchege <cyruschegecc@gmail.com>
2023-07-23 10:50:51 +0300

    Add scripts that add more Flask routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
+    """Defines the home directory"""
     return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Defines the hbnb directory"""
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
